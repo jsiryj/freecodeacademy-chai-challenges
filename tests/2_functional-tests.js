0aaa9e8b4ccbbdfd2cdc448bd483476e3e594f92
Glitch (horse-dirt-lamb) <none>
2020-05-23 09:25:01 +0000

    🎖🌴 Checkpoint ./tests/2_functional-tests.js:15899706/979

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -175,7 +175,7 @@ suite('Functional Tests', function() {
 
   // On Gomix we'll use this setting
   /** ### Copy your project's url here  ### **/
-  Browser.site = ''; 
+  Browser.site = 'https://horse-dirt-lamb.glitch.me'; 
   
   // If you are testing on a local environment replace the line above  with 
   // Browser.localhost('example.com', (process.env.PORT || 3000));
@@ -271,14 +271,20 @@ suite('Functional Tests', function() {
       
       /** Try it again... No help this time **/
       test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
-
+        browser
+        .fill('surname', 'Vespuci')
+        .pressButton('submit', function(){
+                  browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+        done();
+        });
         // fill the form, and submit.
         // assert that status is OK 200
         // assert that the text inside the element 'span#name' is 'Amerigo'
         // assert that the text inside the element 'span#surname' is 'Vespucci'
         // assert that the element(s) 'span#dates' exist and their count is 1
-        assert.fail();
-        done();
       
       });
     });
